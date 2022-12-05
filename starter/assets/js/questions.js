5146e22b22eaec6dcb179eb9a7231e74e8d34dac
Matthew Osborne <momatthewosborne1@gmail.com>
2022-12-05 13:59:30 +0000

    questions addded

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,74 +1,29 @@
 var questions = [
     {
         Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
+        Answers: ['1', '2', '3', '4'],
+        Correct: '1'
     },
     {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
+        Question: "What is 11?",
+        Answers: ['1', '2', '3', '4'],
+        Correct: '1'
     },
     {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
+        Question: "What is 111?",
+        Answers: ['1', '2', '3', '4'],
+        Correct: '1'
     },
     {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
+        Question: "What is 1111?",
+        Answers: ['1', '2', '3', '4'],
+        Correct: '1'
     },
     {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
+        Question: "What is 1 final?",
+        Answers: ['1', '2', '3', '4'],
+        Correct: '1'
     },
-    {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
-    },
-    {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
-    },
-    {
-        Question: "What is 1?",
-        Answers: [
-            { option: "1", answer: true },
-            { option: "2", answer: false },
-            { option: "3", answer: false },
-            { option: "4", answer: false }
-        ]
-    }
+
+
 ]
