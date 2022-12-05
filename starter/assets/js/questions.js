8890f7b63b0a4fefcd8c817cf6c3b2d29c685515
Matthew Osborne <momatthewosborne1@gmail.com>
2022-12-05 18:39:59 +0000

    added questions

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,29 +1,55 @@
 var questions = [
     {
-        Question: "What is 1?",
-        Answers: ['1', '2', '3', '4'],
-        Correct: '1'
+        Question: "What does CSS stand for?",
+        Answers: ['Control Style Selection', 'Cascading Style Sheets', 'Card Catalogue System', 'Create Core Styles'],
+        Correct: 'Cascading Style Sheets'
     },
     {
-        Question: "What is 11?",
-        Answers: ['1', '2', '3', '4'],
-        Correct: '1'
+        Question: "What does HTML stand for?",
+        Answers: ['Hidden Trace Mix Length', 'Helps Make Tables Longer', 'HyperText Makes Links', 'HyperText Markup Language'],
+        Correct: 'HyperText Markup Language'
     },
     {
-        Question: "What is 111?",
-        Answers: ['1', '2', '3', '4'],
-        Correct: '1'
+        Question: "What does DOM stand for?",
+        Answers: ['Document Object Model', 'Document Object Markup', 'Dial Outside Mark', 'Dial Other Mark'],
+        Correct: 'Document Object Model'
     },
     {
-        Question: "What is 1111?",
-        Answers: ['1', '2', '3', '4'],
-        Correct: '1'
+        Question: "What does API stand for?",
+        Answers: ['Allow Programming Instructions', 'Analogue Internet Protocol', 'Application Programming Interface', 'Application Posting Information'],
+        Correct: 'Application Programming Interface'
     },
     {
-        Question: "What is 1 final?",
-        Answers: ['1', '2', '3', '4'],
-        Correct: '1'
+        Question: "Which of these is a Javascript Function?",
+        Answers: ['function{}', 'function[]', 'function<>', 'function()'],
+        Correct: 'function()'
     },
+    {
+        Question: "Where do you link to a CSS file in a HTML Document?",
+        Answers: ['body', 'head', 'footer', 'It does not matter'],
+        Correct: 'head'
+    },
+    {
+        Question: "Where do you link to a JavaScript file in a HTML document?",
+        Answers: ['head', 'body', 'footer', 'It does not matter'],
+        Correct: 'body'
+    },
+    {
+        Question: "How is Java related to Javascript?",
+        Answers: ['They are the same', 'They are not related at all', 'Made by the same person', 'Made by the same team'],
+        Correct: 'They are not related at all'
+    },
+    {
+        Question: "If a website is resposive, it...",
+        Answers: ['has working buttons and links', 'does not crash', 'changes based on device screen size', 'is secure'],
+        Correct: 'changes based on device screen size'
+    },
+    {
+        Question: "Which of these is the correct suffix for a Javascript file?",
+        Answers: ['.java', '.html', '.jv', '.js'],
+        Correct: '.js'
+    }
+
 
 
 ]
